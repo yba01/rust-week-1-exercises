5bc9eb70dc2707fcf2a9510e13d6a605dd9a1aae
yba <byero0470@gmail.com>
2025-06-20 14:49:28 +0000

    first_exercice_really_done

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,10 @@
 pub fn extract_tx_version(raw_tx_hex: &str) -> Result<u32, String> {
     // todo!()
     let transaction = hex::decode(raw_tx_hex);
-    match transaction {
-        Err(_) => return Err("Hex decode error".to_string()),
-        _=>{}
+    if transaction.is_err() {
+        return Err("Hex decode error".to_string());
     }
-    if *&transaction.clone().unwrap().len() < 4 {
+    if transaction.clone().unwrap().len() < 4 {
         return Err("Transaction data too short".to_string());
     }
     let version_byte: [u8; 4] = (&transaction.unwrap()[0..4]).try_into().unwrap();
