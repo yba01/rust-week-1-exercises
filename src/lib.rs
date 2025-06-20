d0c5ea94181419add545ebaa97a55c42a7acf4ec
yba <byero0470@gmail.com>
2025-06-20 14:41:03 +0000

    first_exercice_done

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,15 @@
 // Implement extract_tx_version function below
 pub fn extract_tx_version(raw_tx_hex: &str) -> Result<u32, String> {
-    todo!()
+    // todo!()
+    let transaction = hex::decode(raw_tx_hex);
+    match transaction {
+        Err(_) => return Err("Hex decode error".to_string()),
+        _=>{}
+    }
+    if *&transaction.clone().unwrap().len() < 4 {
+        return Err("Transaction data too short".to_string());
+    }
+    let version_byte: [u8; 4] = (&transaction.unwrap()[0..4]).try_into().unwrap();
+    let version = u32::from_le_bytes(version_byte);
+    Ok(version)
 }
